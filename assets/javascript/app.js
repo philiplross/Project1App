a44563c2cdc37f8d4517aa22d0bc18e21f0ac2e2
Greg Murphy <bopgregorymurphy@gmail.com>
2018-09-29 11:33:55 -0700

    murfstyling

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+
+
+
+//Setting all to zero
+var button = document.createElement("Start");
+button.innerHTML = "startTimer";
+
+let oneTrue = 0;
+let oneFalse = 0;
+let twoTrue = 0;
+let twoFalse = 0;
+let threeTrue = 0;
+let threeFalse = 0;
+let fourTrue = 0;
+let fourFalse = 0;
+let fiveTrue = 0;
+let fiveFalse = 0;
+let sixTrue = 0;
+let sixFalse = 0;
+let sevenTrue = 0;
+let sevenFalse = 0;
+let eightTrue = 0;
+let eightFalse = 0;
+let nineTrue = 0;
+let nineFalse = 0;
+let tenTrue = 0;
+let tenFalse = 0;
+
+let userScore = 0;
+let gameTimer = 0;
+
+
+
